c1dc67c5f9195cc2718caf11c8b993e754682bd0
Zyler <508282+zylerdj@users.noreply.github.com>
2023-03-15 20:07:06 +0000

    Report balance updates via the new API

--- src/balances.ts
+++ src/balances.ts
@@ -1,6 +1,7 @@
 
 require('dotenv').config()
 
+import axios from 'axios';
 import log from './log'
 
 import { load as loadWallet } from './wallet'
@@ -17,6 +18,38 @@ export interface Balance {
   address?: string;
   unspent?: Unspent[];
 }
+
+interface UpdateAddressBalance {
+  chain: string;
+  currency: string;
+  address: string;
+  balance: number;
+  usd_balance?: number;
+}
+
+export async function updateAddressBalance(params: UpdateAddressBalance): Promise<void> {
+
+  const token = String(process.env.anypay_access_token)
+
+  const api_base = `https://api.anypayx.com/v1/api/apps/wallet-bot`
+
+  const { data } = await axios.put(`${api_base}/address-balances`, {
+    chain: params.chain,
+    currency: params.currency,
+    address: params.address,
+    balance: params.balance,
+    usd_balance: params.usd_balance
+  }, {
+    auth: {
+      username: token,
+      password: ''
+    }
+  })
+
+  log.info('updateAddressBalance.result', data)
+
+}
+
 
 export async function listBalances(): Promise<Balance[]> {
 
--- src/websockets/handlers/inventory_balances.ts
+++ src/websockets/handlers/inventory_balances.ts
@@ -1,9 +1,10 @@
 
 import { Socket } from 'socket.io-client'
 
-import { listBalances } from '../../balances'
+import { listBalances, updateAddressBalance } from '../../balances'
 
 import { log } from '../../log'
+
 
 export default async function (socket: Socket, json?: any) {
 
@@ -14,6 +15,22 @@ export default async function (socket: Socket, json?: any) {
     for (let balance of balances) {
 
         log.info('wallet-bot.balance', balance)
+
+        try {
+
+            await updateAddressBalance({
+                address: balance.address,
+                currency: balance.asset,
+                chain: balance.asset,
+                balance: balance.value
+            })
+
+        } catch(error) {
+
+            log.error('wallet-bot.updateAddressBalance.error', error)
+        }
+
+
     }
 
     socket.emit('inventory.balances', balances)
