5be3a32ca43c4b59dde1f703f5bd0c48f222690f
My CTO <mycto@proton.me>
2024-03-31 21:22:40 +0200

    Correct environment variable walletbot_seed_phrase

--- src/wallet.ts
+++ src/wallet.ts
@@ -13,7 +13,7 @@ import config from './config';
 
 export async function load(): Promise<Wallet> {
 
-  const mnemonic = config.get('wallet_bot_backup_seed_phrase')
+  const mnemonic = config.get('walletbot_seed_phrase')
 
   const mnemonicWallet = new MnemonicWallet(mnemonic)
 
